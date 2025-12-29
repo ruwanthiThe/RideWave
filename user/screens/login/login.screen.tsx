ca760ef47c714d6772be80c9666c5c60fe995802
ruwanthiThe <ruthennakoon@gmail.com>
2025-12-29 16:45:18 +0530

    login

--- user/screens/login/login.screen.tsx
+++ user/screens/login/login.screen.tsx
@@ -1,7 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import AuthContainer from '@/utils/container/auth-container'
-import { windowHeight } from '@/themes/app.constant'
+import { View, Text, Image } from "react-native";
+import React, { useState } from "react";
+import AuthContainer from "@/utils/container/auth-container";
+import { windowHeight } from "@/themes/app.constant";
+import styles from "./styles";
+import Images from "@/utils/images";
+import SignInText from "@/components/login/signin.text";
+import { external } from "@/styles/external.style";
+import PhoneNumberInput from "@/components/login/phone-number.input";
+import Button from "@/components/common/button";
+import { router } from "expo-router";
+import { useToast } from "react-native-toast-notifications";
+import axios from "axios";
 
 export default function LoginScreen() {
   return (
@@ -10,6 +19,24 @@ export default function LoginScreen() {
       imageShow={true}
       container={
         <View>
+          <View>
+            <View>
+              <Image style={styles.transformLine} source={Images.line} />
+              <SignInText />
+              <View style={[external.mt_25, external.Pb_10]}>
+                <PhoneNumberInput
+                 
+                />
+                <View style={[external.mt_25, external.Pb_15]}>
+                  <Button
+                    title="Get Otp"
+                    onPress={() => router.push("/(routes)/otp-verification")}
+                    
+                  />
+                </View>
+              </View>
+            </View>
+          </View>
          
         </View>
       }
