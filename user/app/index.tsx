90c3e6a9161fb2bb312e247b662ca7db8e402c6f
ruwanthiThe <ruthennakoon@gmail.com>
2026-01-05 15:25:01 +0530

    commit

--- user/app/index.tsx
+++ user/app/index.tsx
@@ -1,39 +1,45 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Redirect } from "expo-router";
 import { useEffect, useState } from "react";
+import { View, ActivityIndicator } from "react-native";
 
-export default function Index() {
+export default function index() {
   const [isLoggedIn, setisLoggedIn] = useState(false);
   const [isLoading, setisLoading] = useState(true);
 
+  
+
   useEffect(() => {
-    let isMounted = true; // flag to check if the component is mounted
+    
 
     const getData = async () => {
       try {
         const accessToken = await AsyncStorage.getItem("accessToken");
-        if (isMounted) {
-          setisLoggedIn(!!accessToken);
+        if (accessToken) {
+          setisLoggedIn(true);
+        }else{
+          setisLoggedIn(false);
         }
       } catch (error) {
         console.log("Failed to retrieve access token from async storage", error);
       } finally {
-        if (isMounted) {
+        
           setisLoading(false);
         }
-      }
-    };
+      };
+      getData();
+    }, []);
 
-    getData();
+    if (isLoading) {
+      return (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+          <ActivityIndicator size="large" />
+        </View>
+      );
+    }
 
-    return () => {
-      isMounted = false;
-    };
-  }, []);
+    
 
-  if (isLoading) {
-    return null; 
-  }
 
   return (
     <Redirect href={!isLoggedIn ? "/(routes)/onboarding" : "/(tabs)/home"} />
